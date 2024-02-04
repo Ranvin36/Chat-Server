15ac8a66514365d80fb9b31f04b5c9d2a6b660ed
Ranvin36 <141607057+Ranvin36@users.noreply.github.com>
2024-02-04 13:13:25 +0530

    HEY

--- Config/database.js
+++ Config/database.js
@@ -1,7 +1,6 @@
 const  mongoose = require('mongoose');
 
 const mongoDB = async () =>{
-    console.log(process.env.DB_KEY)
     try{
         mongoose.connect(process.env.DB_KEY || "mongodb+srv://ranvin789:9XM1B5SgmSmKLrmo@chat36.caendrp.mongodb.net/?retryWrites=true&w=majority",{
             useNewUrlParser: true,
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 const io = SocketIo(server,{
     cors:{
-        origin:"https://fleexy-chat.netlify.app",
+        origin:"*",
         methods:["GET","POST"]
     }
 })
